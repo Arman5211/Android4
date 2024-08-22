66dc9aaef95b1ac771491750852dadca01a3e89f
Arman <armanhossain3584@gmail.com>
2024-08-22 13:01:27 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/LinearLayoutActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
